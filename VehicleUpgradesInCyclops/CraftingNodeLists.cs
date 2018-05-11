6b6278c818a3e21c0f60eb2cba2d45f2f74b7646
PrimeSonic <prime.sonic@gmail.com>
2018-05-11 07:02:16 -0400

    Updating in preparation of new feature in SML Helper. Complete reorganization of the crafting nodes in the Cyclops fabricator.

---
+++ VehicleUpgradesInCyclops/CraftingNodeLists.cs
@@ -0,0 +1,83 @@
+﻿namespace VehicleUpgradesInCyclops
+{
+    using SMLHelper;
+    using System.Collections.Generic;
+
+    internal static class CraftingNodeLists
+    {
+        internal static IEnumerable<CraftNodeToScrub> OriginalCyclopsModuleCraftingNodes()
+        {
+            return new CraftNodeToScrub[8]
+            {
+                new CraftNodeToScrub(CraftScheme.CyclopsFabricator, "CyclopsHullModule1"),
+                new CraftNodeToScrub(CraftScheme.CyclopsFabricator, "PowerUpgradeModule"),
+                new CraftNodeToScrub(CraftScheme.CyclopsFabricator, "CyclopsShieldModule"),
+                new CraftNodeToScrub(CraftScheme.CyclopsFabricator, "CyclopsSonarModule"),
+                new CraftNodeToScrub(CraftScheme.CyclopsFabricator, "CyclopsSeamothRepairModule"),
+                new CraftNodeToScrub(CraftScheme.CyclopsFabricator, "CyclopsFireSuppressionModule"),
+                new CraftNodeToScrub(CraftScheme.CyclopsFabricator, "CyclopsDecoyModule"),
+                new CraftNodeToScrub(CraftScheme.CyclopsFabricator, "CyclopsThermalReactorModule")
+            };
+        }
+
+        internal static IEnumerable<CustomCraftNode> CyclopsModuleCraftingNodes()
+        {
+            return new CustomCraftNode[8]
+            {
+                new CustomCraftNode(TechType.CyclopsHullModule1, CraftScheme.CyclopsFabricator, "CyclopsModules/CyclopsHullModule1"),
+                new CustomCraftNode(TechType.PowerUpgradeModule, CraftScheme.CyclopsFabricator, "CyclopsModules/PowerUpgradeModule"),
+                new CustomCraftNode(TechType.CyclopsShieldModule, CraftScheme.CyclopsFabricator, "CyclopsModules/CyclopsShieldModule"),
+                new CustomCraftNode(TechType.CyclopsSonarModule, CraftScheme.CyclopsFabricator, "CyclopsModules/CyclopsSonarModule"),
+                new CustomCraftNode(TechType.CyclopsSeamothRepairModule, CraftScheme.CyclopsFabricator, "CyclopsModules/CyclopsSeamothRepairModule"),
+                new CustomCraftNode(TechType.CyclopsFireSuppressionModule, CraftScheme.CyclopsFabricator, "CyclopsModules/CyclopsFireSuppressionModule"),
+                new CustomCraftNode(TechType.CyclopsDecoyModule, CraftScheme.CyclopsFabricator, "CyclopsModules/CyclopsDecoyModule"),
+                new CustomCraftNode(TechType.CyclopsThermalReactorModule, CraftScheme.CyclopsFabricator, "CyclopsModules/CyclopsThermalReactorModule")
+            };
+        }
+
+        internal static IEnumerable<CustomCraftNode> CommonVehicleModuleCraftingNodes()
+        {
+            return new CustomCraftNode[3]
+            {
+                new CustomCraftNode(TechType.VehicleArmorPlating, CraftScheme.CyclopsFabricator, "CommonModules/VehicleArmorPlating"),
+                new CustomCraftNode(TechType.VehiclePowerUpgradeModule, CraftScheme.CyclopsFabricator, "CommonModules/VehiclePowerUpgradeModule"),
+                new CustomCraftNode(TechType.VehicleStorageModule, CraftScheme.CyclopsFabricator, "CommonModules/VehicleStorageModule")
+            };
+        }
+
+        internal static IEnumerable<CustomCraftNode> SeamothModuleCraftingNodes()
+        {
+            return new CustomCraftNode[5]
+            {
+                new CustomCraftNode(TechType.VehicleHullModule1, CraftScheme.CyclopsFabricator, "SeamothModules/VehicleHullModule1"),
+                new CustomCraftNode(TechType.SeamothSolarCharge, CraftScheme.CyclopsFabricator, "SeamothModules/SeamothSolarCharge"),
+                new CustomCraftNode(TechType.SeamothElectricalDefense, CraftScheme.CyclopsFabricator, "SeamothModules/SeamothElectricalDefense"),
+                new CustomCraftNode(TechType.SeamothTorpedoModule, CraftScheme.CyclopsFabricator, "SeamothModules/SeamothTorpedoModule"),
+                new CustomCraftNode(TechType.SeamothSonarModule, CraftScheme.CyclopsFabricator, "SeamothModules/SeamothSonarModule"),
+            };
+        }
+
+        internal static IEnumerable<CustomCraftNode> ExosuitModuleCraftingNodes()
+        {
+            return new CustomCraftNode[7]
+            {
+                new CustomCraftNode(TechType.ExoHullModule1, CraftScheme.CyclopsFabricator, "ExosuitModules/ExoHullModule1"),
+                new CustomCraftNode(TechType.ExosuitThermalReactorModule, CraftScheme.CyclopsFabricator, "ExosuitModules/ExosuitThermalReactorModule"),
+                new CustomCraftNode(TechType.ExosuitJetUpgradeModule, CraftScheme.CyclopsFabricator, "ExosuitModules/ExosuitJetUpgradeModule"),
+                new CustomCraftNode(TechType.ExosuitPropulsionArmModule, CraftScheme.CyclopsFabricator, "ExosuitModules/ExosuitPropulsionArmModule"),
+                new CustomCraftNode(TechType.ExosuitGrapplingArmModule, CraftScheme.CyclopsFabricator, "ExosuitModules/ExosuitGrapplingArmModule"),
+                new CustomCraftNode(TechType.ExosuitDrillArmModule, CraftScheme.CyclopsFabricator, "ExosuitModules/ExosuitDrillArmModule"),
+                new CustomCraftNode(TechType.ExosuitTorpedoArmModule, CraftScheme.CyclopsFabricator, "ExosuitModules/ExosuitTorpedoArmModule")
+            };
+        }
+
+        internal static IEnumerable<CustomCraftNode> TorpedoCraftingNodes()
+        {
+            return new CustomCraftNode[2]
+            {
+                new CustomCraftNode(TechType.WhirlpoolTorpedo, CraftScheme.CyclopsFabricator, "Torpedoes/WhirlpoolTorpedo"),
+                new CustomCraftNode(TechType.GasTorpedo, CraftScheme.CyclopsFabricator, "Torpedoes/GasTorpedo")
+            };
+        }
+    }
+}
--- VehicleUpgradesInCyclops/QPatch.cs
+++ VehicleUpgradesInCyclops/QPatch.cs
@@ -1,13 +1,12 @@
 ﻿namespace VehicleUpgradesInCyclops
-{    
+{
     using SMLHelper; // https://github.com/ahk1221/SMLHelper/
     using SMLHelper.Patchers;
-
+    
 #if DEBUG
     using System;
     using Logger = Utilites.Logger.Logger;
-    // TODO Custom sprites for an look and feel more similar to the actual Vehicle Upgrade Console.
-    // TODO Do something about the icons going off screen due to sheer numbers when all the Cyclops upgrades are unlocked.
+    // TODO Custom sprites for an look and feel more similar to the actual Vehicle Upgrade Console.    
 #endif
     public class QPatch
     {
@@ -16,41 +15,32 @@
 #if DEBUG
             try
             {
-                Logger.Debug("Begin patching");
+                Logger.Debug("Loading Crafting Nodes - Start");
 #endif
-                // Small Vehicle Upgrades (root tab to everything below)
-                CraftTreePatcher.customTabs.Add(new CustomCraftTab("VehicleUpgrades", "Small Vehicle Upgrades", CraftScheme.CyclopsFabricator, SpriteManager.Get(TechType.BaseUpgradeConsole)));
-               
-                // Prawn Suit Modules
-                CraftTreePatcher.customTabs.Add(new CustomCraftTab("VehicleUpgrades/PrawnModules", "Prawn Suit Modules", CraftScheme.CyclopsFabricator, SpriteManager.Get(TechType.Exosuit)));
-                CraftTreePatcher.customNodes.Add(new CustomCraftNode(TechType.ExoHullModule1, CraftScheme.CyclopsFabricator, "VehicleUpgrades/PrawnModules/ExoHullModule1"));
-                CraftTreePatcher.customNodes.Add(new CustomCraftNode(TechType.ExosuitThermalReactorModule, CraftScheme.CyclopsFabricator, "VehicleUpgrades/PrawnModules/ExosuitThermalReactorModule"));
-                CraftTreePatcher.customNodes.Add(new CustomCraftNode(TechType.ExosuitJetUpgradeModule, CraftScheme.CyclopsFabricator, "VehicleUpgrades/PrawnModules/ExosuitJetUpgradeModule"));
-                CraftTreePatcher.customNodes.Add(new CustomCraftNode(TechType.ExosuitPropulsionArmModule, CraftScheme.CyclopsFabricator, "VehicleUpgrades/PrawnModules/ExosuitPropulsionArmModule"));
-                CraftTreePatcher.customNodes.Add(new CustomCraftNode(TechType.ExosuitGrapplingArmModule, CraftScheme.CyclopsFabricator, "VehicleUpgrades/PrawnModules/ExosuitGrapplingArmModule"));
-                CraftTreePatcher.customNodes.Add(new CustomCraftNode(TechType.ExosuitDrillArmModule, CraftScheme.CyclopsFabricator, "VehicleUpgrades/PrawnModules/ExosuitDrillArmModule"));
-                CraftTreePatcher.customNodes.Add(new CustomCraftNode(TechType.ExosuitTorpedoArmModule, CraftScheme.CyclopsFabricator, "VehicleUpgrades/PrawnModules/ExosuitTorpedoArmModule"));
+                // Remove all original Cyclops fabricator nodes
+                CraftTreePatcher.nodesToRemove.AddRange(CraftingNodeLists.OriginalCyclopsModuleCraftingNodes());
 
-                // Seamoth Modules
-                CraftTreePatcher.customTabs.Add(new CustomCraftTab("VehicleUpgrades/SeamothModules", "Seamoth Modules", CraftScheme.CyclopsFabricator, SpriteManager.Get(TechType.Seamoth)));
-                CraftTreePatcher.customNodes.Add(new CustomCraftNode(TechType.VehicleHullModule1, CraftScheme.CyclopsFabricator, "VehicleUpgrades/SeamothModules/VehicleHullModule1"));
-                CraftTreePatcher.customNodes.Add(new CustomCraftNode(TechType.SeamothSolarCharge, CraftScheme.CyclopsFabricator, "VehicleUpgrades/SeamothModules/SeamothSolarCharge"));
-                CraftTreePatcher.customNodes.Add(new CustomCraftNode(TechType.SeamothElectricalDefense, CraftScheme.CyclopsFabricator, "VehicleUpgrades/SeamothModules/SeamothElectricalDefense"));
-                CraftTreePatcher.customNodes.Add(new CustomCraftNode(TechType.SeamothTorpedoModule, CraftScheme.CyclopsFabricator, "VehicleUpgrades/SeamothModules/SeamothTorpedoModule"));
-                CraftTreePatcher.customNodes.Add(new CustomCraftNode(TechType.SeamothSonarModule, CraftScheme.CyclopsFabricator, "VehicleUpgrades/SeamothModules/SeamothSonarModule"));
+                // New Cyclops Upgrades Tab (This will keep things more organized)
+                CraftTreePatcher.customTabs.Add(new CustomCraftTab("CyclopsModules", "Cyclops Modules", CraftScheme.CyclopsFabricator, SpriteManager.Get(TechType.Cyclops)));
+                CraftTreePatcher.customNodes.AddRange(CraftingNodeLists.CyclopsModuleCraftingNodes());
 
                 // Common Modules
-                CraftTreePatcher.customTabs.Add(new CustomCraftTab("VehicleUpgrades/CommonModules", "Common Modules", CraftScheme.CyclopsFabricator, SpriteManager.Get(TechType.VehicleStorageModule)));
-                CraftTreePatcher.customNodes.Add(new CustomCraftNode(TechType.VehicleArmorPlating, CraftScheme.CyclopsFabricator, "VehicleUpgrades/CommonModules/VehicleArmorPlating"));
-                CraftTreePatcher.customNodes.Add(new CustomCraftNode(TechType.VehiclePowerUpgradeModule, CraftScheme.CyclopsFabricator, "VehicleUpgrades/CommonModules/VehiclePowerUpgradeModule"));
-                CraftTreePatcher.customNodes.Add(new CustomCraftNode(TechType.VehicleStorageModule, CraftScheme.CyclopsFabricator, "VehicleUpgrades/CommonModules/VehicleStorageModule"));
+                CraftTreePatcher.customTabs.Add(new CustomCraftTab("CommonModules", "Common Modules", CraftScheme.CyclopsFabricator, SpriteManager.Get(TechType.VehicleStorageModule)));
+                CraftTreePatcher.customNodes.AddRange(CraftingNodeLists.CommonVehicleModuleCraftingNodes());
+
+                // Seamoth Modules
+                CraftTreePatcher.customTabs.Add(new CustomCraftTab("SeamothModules", "Seamoth Modules", CraftScheme.CyclopsFabricator, SpriteManager.Get(TechType.Seamoth)));
+                CraftTreePatcher.customNodes.AddRange(CraftingNodeLists.SeamothModuleCraftingNodes());
+
+                // Prawn Suit Modules
+                CraftTreePatcher.customTabs.Add(new CustomCraftTab("ExosuitModules", "Prawn Suit Modules", CraftScheme.CyclopsFabricator, SpriteManager.Get(TechType.Exosuit)));
+                CraftTreePatcher.customNodes.AddRange(CraftingNodeLists.ExosuitModuleCraftingNodes());
 
                 // Torpedoes
-                CraftTreePatcher.customTabs.Add(new CustomCraftTab("VehicleUpgrades/Torpedoes", "Torpedoes", CraftScheme.CyclopsFabricator, SpriteManager.Get(TechType.SeamothTorpedoModule)));
-                CraftTreePatcher.customNodes.Add(new CustomCraftNode(TechType.WhirlpoolTorpedo, CraftScheme.CyclopsFabricator, "VehicleUpgrades/Torpedoes/WhirlpoolTorpedo"));
-                CraftTreePatcher.customNodes.Add(new CustomCraftNode(TechType.GasTorpedo, CraftScheme.CyclopsFabricator, "VehicleUpgrades/Torpedoes/GasTorpedo"));
+                CraftTreePatcher.customTabs.Add(new CustomCraftTab("Torpedoes", "Torpedoes", CraftScheme.CyclopsFabricator, SpriteManager.Get(TechType.SeamothTorpedoModule)));
+                CraftTreePatcher.customNodes.AddRange(CraftingNodeLists.TorpedoCraftingNodes());
 #if DEBUG
-                Logger.Debug("Patching complete");
+                Logger.Debug("Loading Crafting Nodes - Completes");
             }
             catch (Exception ex)
             {
