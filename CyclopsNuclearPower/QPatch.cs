10c6ef6b008a8d0c13e67ade2af41515883e6ee6
PrimeSonic <prime.sonic@gmail.com>
2018-06-01 23:14:26 -0400

    CyclopsNuclerPower - Split patcher code for clarity and organization - Should be able to support multiple Cyclops in the game each with nuclear batteries - Correctly starts powering up again after loading a save - In a good state to release minus the pending icon.

--- CyclopsNuclearPower/QPatch.cs
+++ CyclopsNuclearPower/QPatch.cs
@@ -11,11 +11,9 @@
     // QMods by qwiso https://github.com/Qwiso/QModManager
     public class QPatch
     {
-        internal const string ModFolder = @"./QMods/NuclearCyclops";
-
         public static TechType CyReactorRodType { get; private set; }
         public const string NameId = "CyclopsNuclearModule";
-        public const string FriendlyName = "Cyclops Nuclear Module";
+        public const string FriendlyName = "Cyclops Nuclear Battery Module";
 
         public static void Patch()
         {
@@ -28,15 +26,37 @@
         private static void CreateItem()
         {
             // Create a new TechType
-            CyReactorRodType = TechTypePatcher.AddTechType("CyclopsReactorRod", FriendlyName, "Portable nuclear power for your Cyclops. Handle with care.", true);
+            CyReactorRodType = TechTypePatcher.AddTechType(NameId, FriendlyName, "Portable nuclear power for your Cyclops. The days of running out of power are over.", true);
+            
+            // Make sure it's available from the start, it's hard enough to get as it is.
+            KnownTechPatcher.unlockedAtStart.Add(CyReactorRodType);
 
-            // Create a new Recipie
-            var cyReactorRodRecipe = new TechDataHelper()
+            // Create the in-game item that will behave like any other Cyclops upgrade module
+            CustomPrefabHandler.customPrefabs.Add(new CustomPrefab(NameId, $"WorldEntities/Tools/{NameId}", CyReactorRodType, GetGameObject));
+
+            // Get the custom icon from the Unity assets bundle
+            CustomSpriteHandler.customSprites.Add(new CustomSprite(CyReactorRodType, SpriteManager.Get(TechType.ReactorRod)));
+            // TODO - Custom Sprite  - AssetBundle.LoadFromFile($"{ModFolder}/Assets/cysolar.assets").LoadAsset<Sprite>("CySolarIcon")));
+
+            // Add the new recipe to the Modification Station crafting tree
+            CraftTreePatcher.customNodes.Add(new CustomCraftNode(CyReactorRodType, CraftTree.Type.Workbench, $"CyclopsMenu/{NameId}"));
+
+            // Pair the new recipie with the new TechType
+            CraftDataPatcher.customTechData[CyReactorRodType] = GetRecipe();
+
+            // Ensure that the new in-game item is classified as a Cyclops upgrade module. Otherwise you can't equip it.
+            CraftDataPatcher.customEquipmentTypes[CyReactorRodType] = EquipmentType.CyclopsModule;
+        }
+
+        private static TechDataHelper GetRecipe()
+        {            
+            return new TechDataHelper()
             {
                 _craftAmount = 1,
                 _ingredients = new List<IngredientHelper>(new IngredientHelper[6]
                              {
-                                 new IngredientHelper(TechType.ReactorRod, 1),
+                                 // This is to validate that the player has access to nuclear power already
+                                 new IngredientHelper(TechType.ReactorRod, 1), 
                                  new IngredientHelper(TechType.Lubricant, 2),
                                  new IngredientHelper(TechType.EnameledGlass, 2),
                                  new IngredientHelper(TechType.ComputerChip, 1),
@@ -45,25 +65,6 @@
                              }),
                 _techType = CyReactorRodType
             };
-
-            // Make sure it's available from the start, it's hard enough to get as it is.
-            KnownTechPatcher.unlockedAtStart.Add(CyReactorRodType);
-
-            // Create the in-game item that will behave like any other Cyclops upgrade module
-            CustomPrefabHandler.customPrefabs.Add(new CustomPrefab(NameId, "WorldEntities/Tools/CyclopsReactorRod", CyReactorRodType, GetGameObject));
-
-            // Get the custom icon from the Unity assets bundle
-            CustomSpriteHandler.customSprites.Add(new CustomSprite(CyReactorRodType, SpriteManager.Get(TechType.ReactorRod)));
-            // TODO - Custom Sprite  - AssetBundle.LoadFromFile($"{ModFolder}/Assets/cysolar.assets").LoadAsset<Sprite>("CySolarIcon")));
-
-            // Add the new recipe to the Modification Station crafting tree
-            CraftTreePatcher.customNodes.Add(new CustomCraftNode(CyReactorRodType, CraftTree.Type.Workbench, "CyclopsMenu/CyclopsReactorRod"));
-
-            // Pair the new recipie with the new TechType
-            CraftDataPatcher.customTechData[CyReactorRodType] = cyReactorRodRecipe;
-
-            // Ensure that the new in-game item is classified as a Cyclops upgrade module. Otherwise you can't equip it.
-            CraftDataPatcher.customEquipmentTypes[CyReactorRodType] = EquipmentType.CyclopsModule;
         }
 
         private static GameObject GetGameObject()
@@ -74,11 +75,11 @@
             obj.GetComponent<PrefabIdentifier>().ClassId = NameId;
             obj.GetComponent<TechTag>().type = CyReactorRodType;
 
-            // Using an actual Battery component means the game just handles saving the data.
+            // The battery component makes it easy to track the charge and saving the data is automatic.
             var pCell = obj.AddComponent<Battery>();
             pCell.name = FriendlyName;
-            pCell._capacity = CyNukReactor.MaxCharge;
-            pCell._charge = CyNukReactor.MaxCharge;
+            pCell._capacity = NuclearBatteryManager.MaxCharge;
+            pCell._charge = NuclearBatteryManager.MaxCharge;
 
             return obj;
         }
---
+++ CyclopsNuclearPower/SubRoot_UpdateThermalReactorCharge_Patcher.cs
@@ -0,0 +1,35 @@
+﻿namespace CyclopsNuclearPower
+{
+    using Harmony;
+
+    [HarmonyPatch(typeof(SubRoot))]
+    [HarmonyPatch("UpdateThermalReactorCharge")]
+    internal class SubRoot_UpdateThermalReactorCharge_Patcher
+    {
+        /// <summary>
+        /// This patch method handles actually providing charge to the Cyclops 
+        /// while the <see cref="NuclearBatteryManager"/> tracks the nuclear battery charge.
+        /// </summary>        
+        public static void Postfix(ref SubRoot __instance)
+        {
+            if (__instance.upgradeConsole == null)
+            {
+                return; // mimicing safety conditions from SetCyclopsUpgrades() method in SubRoot
+            }
+
+            bool cyclopsHasPowerCells = __instance.powerRelay.GetPowerStatus() == PowerSystem.Status.Normal;
+
+            if (!cyclopsHasPowerCells)
+            {
+                // Don't drain if there are no batteries to charge
+                // Potential issue if the player somehow managed to actually drain all their power cells
+                return;
+            }
+
+            NuclearBatteryManager.UpdateNuclearBatteryCharges(ref __instance);
+        }
+
+
+    }
+
+}
