156cf1993f0a36b9cf00a9e16f34befad9572cd5
PrimeSonic <prime.sonic@gmail.com>
2018-06-12 22:16:19 -0400

    CustomCraftSML - Completion of initial EasyMarkup library for serialization

---
+++ CustomCraftSML/Serialization/EasyMarkup/DoubleQueue.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CustomCraftSML.Serialization.EasyMarkup
+{
+    public class DoubleQueue<T> : LinkedList<T>
+    {
+        public T PeekStart() => base.First.Value;
+        public T PeekEnd() => base.Last.Value;
+
+        public T RemoveFromStart()
+        {
+            T value = base.First.Value;
+
+            base.RemoveFirst();
+
+            return value;
+        }
+
+        public T RemoveFromEnd()
+        {
+            T value = base.Last.Value;
+
+            base.RemoveLast();
+
+            return value;
+        }
+
+        public bool RemoveFromStart(T value)
+        {
+            if (base.First.Value.Equals(value))
+            {
+                base.RemoveFirst();
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool RemoveFromEnd(T value)
+        {
+            if (base.Last.Value.Equals(value))
+            {
+                base.RemoveLast();
+                return true;
+            }
+
+            return false;
+        }
+
+        public void AddFromStart(T value) => base.AddFirst(value);
+        public void AddFromEnd(T value) => base.AddLast(value);
+    }
+}
---
+++ CustomCraftSML/Serialization/EasyMarkup/StringBuffer.cs
@@ -0,0 +1,30 @@
+﻿namespace CustomCraftSML.Serialization.EasyMarkup
+{
+    public class StringBuffer : DoubleQueue<char>
+    {
+        public StringBuffer()
+        {
+
+        }
+
+        public StringBuffer(char[] array)
+        {
+            foreach (char c in array)
+                base.AddLast(c);
+        }
+
+        public override string ToString()
+        {
+            char[] array = new char[base.Count];
+
+            int i = 0;
+            foreach (char c in this)
+            {
+                array[i] = c;
+                i++;
+            }
+
+            return new string(array);
+        }
+    }
+}
