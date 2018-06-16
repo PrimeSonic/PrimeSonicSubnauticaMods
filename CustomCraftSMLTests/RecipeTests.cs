c047a5456b7e870dac7741494322d8874da759a6
PrimeSonic <prime.sonic@gmail.com>
2018-06-15 23:15:56 -0400

    Full Unit test

--- CustomCraftSMLTests/RecipeTests.cs
+++ CustomCraftSMLTests/RecipeTests.cs
@@ -30,7 +30,20 @@
 
             recipe.FromString(serialized);
 
+            Assert.AreEqual(TechType.Aerogel, recipe.ItemID);
+            Assert.AreEqual(1, recipe.AmountCrafted);
 
+            Assert.AreEqual(2, recipe.Ingredients.Count);
+            Assert.AreEqual(TechType.Titanium, recipe.Ingredients[0].ItemID);
+            Assert.AreEqual(2, recipe.Ingredients[0].Required);
+            Assert.AreEqual(TechType.Copper, recipe.Ingredients[1].ItemID);
+            Assert.AreEqual(3, recipe.Ingredients[1].Required);
+
+            Assert.AreEqual(2, recipe.LinkedItems.Count);
+            Assert.AreEqual(TechType.Silver, recipe.LinkedItems[0]);
+            Assert.AreEqual(TechType.Gold, recipe.LinkedItems[1]);
+
+            Assert.AreEqual("Fabricator/Resources/BasicMaterials", recipe.Path);
         }
 
 
