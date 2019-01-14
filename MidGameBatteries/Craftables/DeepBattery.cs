6bc8c0b067a315867c6d217699a706c61b09f912
PrimeSonic <prime.sonic@gmail.com>
2019-01-13 21:25:52 -0500

    MidGameBatConfig - The amount of energy capacity in the DeepBattery and DeepPowerCell are now user configurable - Updates MidGameBatteries to 1.1.0

--- MidGameBatteries/Craftables/DeepBattery.cs
+++ MidGameBatteries/Craftables/DeepBattery.cs
@@ -6,19 +6,20 @@
     internal class DeepBattery : DeepBatteryCellBase
     {
         // This battery provides 2.5x the power of a normal battery
-        internal const float BatteryCapacity = 250f;
+        internal static float BatteryCapacity { get; private set; } = 250f;
 
-        public DeepBattery()
+        public DeepBattery(float capacity = 250f)
             : base(classId: "DeepBattery",
                    friendlyName: "Deep Battery",
                    description: "A longer lasting battery created from rare materials and stronger chemicals.")
         {
+            BatteryCapacity = capacity;
             // This event will be invoked after all patching done by the Craftable class is complete
             OnFinishedPatching += SetStaticTechType;
         }
 
         protected override TechType BaseType { get; } = TechType.LithiumIonBattery;
-        protected override float PowerCapacity { get; } = BatteryCapacity;
+        protected override float PowerCapacity => BatteryCapacity;
         protected override EquipmentType ChargerType { get; } = EquipmentType.BatteryCharger;
 
         protected override TechData GetBlueprintRecipe()
---
+++ MidGameBatteries/DeepConfig.cs
@@ -0,0 +1,124 @@
+﻿namespace MidGameBatteries
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+    using Common;
+    using Common.EasyMarkup;
+
+    internal enum PowerStyle
+    {
+        VeryLow,
+        Low,
+        Normal,
+        High,
+        VeryHigh,
+    }
+
+    internal class DeepConfig : EmPropertyCollection
+    {
+        private const string MainKey = "MidGameBatConfig";
+        private const string PowerStyleKey = "BatteryPower";
+        private const float BasePower = 100f;
+
+        private const string ConfigDirectory = "./QMods/MidGameBatteries";
+        private const string ConfigFile = ConfigDirectory + "/" + MainKey + ".txt";
+
+        private readonly EmProperty<PowerStyle> PowerStyleConfig;
+
+        private static ICollection<EmProperty> DeepConfigDefinitions => new List<EmProperty>
+        {
+            new EmProperty<PowerStyle>(PowerStyleKey),
+        };
+
+        public DeepConfig() : base(MainKey, DeepConfigDefinitions)
+        {
+            PowerStyleConfig = (EmProperty<PowerStyle>)Properties[PowerStyleKey];
+            PowerStyleConfig.Value = PowerStyle.Normal;
+        }
+
+        public PowerStyle SelectedPowerStyle
+        {
+            get => PowerStyleConfig.Value;
+            set => PowerStyleConfig.Value = value;
+        }
+
+        public float BatteryCapacity
+        {
+            get
+            {
+                switch (SelectedPowerStyle)
+                {
+                    case PowerStyle.VeryLow: return BasePower * 1.5f;
+                    case PowerStyle.Low: return BasePower * 2f;
+                    case PowerStyle.Normal: return BasePower * 2.5f;
+                    case PowerStyle.High: return BasePower * 3f;
+                    case PowerStyle.VeryHigh: return BasePower * 3.75f;
+                    default: return BasePower * 1.75f;
+                }
+            }
+        }
+
+        internal override EmProperty Copy() => new DeepConfig() { SelectedPowerStyle = this.SelectedPowerStyle };
+
+        internal void ReadConfigFile()
+        {
+            try
+            {
+                Load();
+            }
+            catch (Exception ex)
+            {
+                QuickLogger.Message($"Error loading {MainKey}: " + ex.ToString());
+                Save();
+            }
+        }
+
+        public void Save()
+        {
+            File.WriteAllLines(ConfigFile, new[]
+            {
+                "# --------------------------------------------------------------------------- #",
+                "# Updates to this config file will ONLY take effect AFTER restarting the game #",
+                "#                 This config file was built using EasyMarkup                 #",
+                "# --------------------------------------------------------------------------- #",
+                "",
+                "# You can configure BatteryPower to customize how much extra power you'll get out of the Deep Battery and Deep Power Cell. #",
+                "# Choose one of these options:                                                                                             #",
+                "",
+                "# VeryLow  : 1.5X more power than the standard batteries. Suitable for players who want a some upgrade but still want to worry about managing energy. #",
+                "# Low      :   2X more power than the standard batteries. For players who really want to feel the jump without feeling like they got too much out.    #",
+                "# Normal   : 2.5X more power than the standard batteries. The default recommended setting. Exactly half-way between the standard and ion batteries.   #",
+                "# High     :   3X more power than the standard batteries. For players who feel the ion batteries come too late in the game to be useful.              #",
+                // VeryHigh : 3.75X - EasterEgg for players who don't want to have to think about energy
+                "",
+                PrettyPrint(),
+                "",
+                "# NOTE: Changing this setting will only affect new Deep Batteries and Deep Power Cells you craft. #",
+                "# Any existing batteries already in your game will not be changed.                                #",
+            },
+            Encoding.UTF8);
+        }
+
+        private void Load()
+        {
+            if (!File.Exists(ConfigFile))
+            {
+                QuickLogger.Message($"Config file not found. Writing default file.");
+                Save();
+                return;
+            }
+
+            string text = File.ReadAllText(ConfigFile, Encoding.UTF8);
+
+            bool readCorrectly = FromString(text);
+
+            if (!readCorrectly)
+            {
+                QuickLogger.Message($"Config file contained errors. Writing default file.");
+                Save();
+            }
+        }
+    }
+}
