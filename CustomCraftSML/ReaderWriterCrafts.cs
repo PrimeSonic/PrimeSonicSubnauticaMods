eb97c539418d9fd533d92c63b764ee480a0dfedc
PrimeSonic <prime.sonic@gmail.com>
2019-02-16 00:22:53 -0500

    CC2_164 - Extracted much of ReaderWriterCrafts into a new ParsingPackage class

--- CustomCraftSML/ReaderWriterCrafts.cs
+++ CustomCraftSML/ReaderWriterCrafts.cs
@@ -4,7 +4,7 @@
     using System.IO;
     using Common;
     using Common.EasyMarkup;
-    using CustomCraft2SML.Interfaces;
+    using CustomCraft2SML.Serialization;
     using CustomCraft2SML.Serialization.Entries;
     using CustomCraft2SML.Serialization.Lists;
 
@@ -13,27 +13,32 @@
         internal const string WorkingFolder = FolderRoot + "WorkingFiles/";
         internal const string AssetsFolder = FolderRoot + "Assets/";
 
-        //  Initial storage for the serialization - key is string as we have not resolved the TechType at this point
-        private static readonly IList<MovedRecipe> movedRecipes = new List<MovedRecipe>();
-        private static readonly IList<AddedRecipe> addedRecipes = new List<AddedRecipe>();
-        private static readonly IList<AliasRecipe> aliasRecipes = new List<AliasRecipe>();
-        private static readonly IList<ModifiedRecipe> modifiedRecipes = new List<ModifiedRecipe>();
-        private static readonly IList<CustomSize> customSizes = new List<CustomSize>();
-        private static readonly IList<CustomBioFuel> customBioFuels = new List<CustomBioFuel>();
-        private static readonly IList<CustomFragmentCount> customFragments = new List<CustomFragmentCount>();
-        private static readonly IList<CustomCraftingTab> customTabs = new List<CustomCraftingTab>();
+        private static readonly IParsingPackage CustomCraftingTabs = new ParsingPackage<CustomCraftingTab, CustomCraftingTabList>(CustomCraftingTabList.ListKey);
+        private static readonly IParsingPackage MovedRecipes = new ParsingPackage<MovedRecipe, MovedRecipeList>(MovedRecipeList.ListKey);
+        private static readonly IParsingPackage AddedRecipes = new ParsingPackage<AddedRecipe, AddedRecipeList>(AddedRecipeList.ListKey);
+        private static readonly IParsingPackage AliasRecipes = new ParsingPackage<AliasRecipe, AliasRecipeList>(AliasRecipeList.ListKey);
+        private static readonly IParsingPackage ModifiedRecipes = new ParsingPackage<ModifiedRecipe, ModifiedRecipeList>(ModifiedRecipeList.ListKey);
+        private static readonly IParsingPackage CustomSizes = new ParsingPackage<CustomSize, CustomSizeList>(CustomSizeList.ListKey);
+        private static readonly IParsingPackage CustomBioFuels = new ParsingPackage<CustomBioFuel, CustomBioFuelList>(CustomBioFuelList.ListKey);
+        private static readonly IParsingPackage CustomFragmentCounts = new ParsingPackage<CustomFragmentCount, CustomFragmentCountList>(CustomFragmentCountList.ListKey);
 
-        private static readonly IDictionary<string, CustomCraftingTab> uniqueCustomTabs = new Dictionary<string, CustomCraftingTab>();
-        private static readonly IDictionary<string, MovedRecipe> uniqueMovedRecipes = new Dictionary<string, MovedRecipe>();
-        private static readonly IDictionary<string, AddedRecipe> uniqueAddedRecipes = new Dictionary<string, AddedRecipe>();
-        private static readonly IDictionary<string, AliasRecipe> uniqueAliasRecipes = new Dictionary<string, AliasRecipe>();
-        private static readonly IDictionary<string, ModifiedRecipe> uniqueModifiedRecipes = new Dictionary<string, ModifiedRecipe>();
-        private static readonly IDictionary<string, CustomSize> uniqueCustomSizes = new Dictionary<string, CustomSize>();
-        private static readonly IDictionary<string, CustomBioFuel> uniqueCustomBioFuels = new Dictionary<string, CustomBioFuel>();
-        private static readonly IDictionary<string, CustomFragmentCount> uniqueCustomFragments = new Dictionary<string, CustomFragmentCount>();
+        private static IList<IParsingPackage> OrderedPackages = new List<IParsingPackage>();
+        private static IDictionary<string, IParsingPackage> LookupPackages = new Dictionary<string, IParsingPackage>();
 
         private static void HandleWorkingFiles()
         {
+            OrderedPackages.Add(CustomCraftingTabs);
+            OrderedPackages.Add(MovedRecipes);
+            OrderedPackages.Add(AddedRecipes);
+            OrderedPackages.Add(AliasRecipes);
+            OrderedPackages.Add(ModifiedRecipes);
+            OrderedPackages.Add(CustomSizes);
+            OrderedPackages.Add(CustomBioFuels);
+            OrderedPackages.Add(CustomFragmentCounts);
+
+            foreach (IParsingPackage package in OrderedPackages)
+                LookupPackages.Add(package.ListKey, package);
+
             if (!Directory.Exists(AssetsFolder))
                 Directory.CreateDirectory(AssetsFolder);
 
@@ -46,23 +51,15 @@
             foreach (string file in workingFiles)
                 DeserializeFile(file);
 
-            PrePassValidation(customTabs, uniqueCustomTabs);
-            PrePassValidation(movedRecipes, uniqueMovedRecipes);
-            PrePassValidation(addedRecipes, uniqueAddedRecipes);
-            PrePassValidation(aliasRecipes, uniqueAliasRecipes);
-            PrePassValidation(modifiedRecipes, uniqueModifiedRecipes);
-            PrePassValidation(customSizes, uniqueCustomSizes);
-            PrePassValidation(customBioFuels, uniqueCustomBioFuels);
-            PrePassValidation(customFragments, uniqueCustomFragments);
+            QuickLogger.Message($"Validating parsed entries");
 
-            SendToSMLHelper(uniqueCustomTabs.Values);
-            SendToSMLHelper(uniqueMovedRecipes.Values);
-            SendToSMLHelper(uniqueAddedRecipes.Values);
-            SendToSMLHelper(uniqueAliasRecipes.Values);
-            SendToSMLHelper(uniqueModifiedRecipes.Values);
-            SendToSMLHelper(uniqueCustomSizes.Values);
-            SendToSMLHelper(uniqueCustomBioFuels.Values);
-            SendToSMLHelper(uniqueCustomFragments.Values);
+            foreach (IParsingPackage package in OrderedPackages)
+                package.PrePassValidation();
+
+            QuickLogger.Message($"Sending requests to SMLHelper");
+
+            foreach (IParsingPackage package in OrderedPackages)
+                package.SendToSMLHelper();
         }
 
         private static void DeserializeFile(string workingFilePath)
@@ -80,47 +77,21 @@
             if (EmProperty.CheckKey(serializedData, out string key))
             {
                 int check = -2;
-                switch (key)
+                if (LookupPackages.TryGetValue(key, out IParsingPackage package))
                 {
-                    case AddedRecipeList.ListKey:
-                        check = ParseEntries<AddedRecipe, AddedRecipeList>(serializedData, addedRecipes);
-                        break;
-
-                    case AliasRecipeList.ListKey:
-                        check = ParseEntries<AliasRecipe, AliasRecipeList>(serializedData, aliasRecipes);
-                        break;
-
-                    case ModifiedRecipeList.ListKey:
-                        check = ParseEntries<ModifiedRecipe, ModifiedRecipeList>(serializedData, modifiedRecipes);
-                        break;
-
-                    case CustomSizeList.ListKey:
-                        check = ParseEntries<CustomSize, CustomSizeList>(serializedData, customSizes);
-                        break;
-
-                    case CustomBioFuelList.ListKey:
-                        check = ParseEntries<CustomBioFuel, CustomBioFuelList>(serializedData, customBioFuels);
-                        break;
-
-                    case CustomCraftingTabList.ListKey:
-                        check = ParseEntries<CustomCraftingTab, CustomCraftingTabList>(serializedData, customTabs);
-                        break;
-
-                    case MovedRecipeList.ListKey:
-                        check = ParseEntries<MovedRecipe, MovedRecipeList>(serializedData, movedRecipes);
-                        break;
-
-                    case CustomFragmentCountList.ListKey:
-                        check = ParseEntries<CustomFragmentCount, CustomFragmentCountList>(serializedData, customFragments);
-                        break;
-
-                    default:
-                        QuickLogger.Error($"Unknown primary key '{key}' detected in file '{fileName}'");
-                        return;
+                    check = package.ParseEntries(serializedData);
+                }
+                else
+                {
+                    QuickLogger.Error($"Unknown primary key '{key}' detected in file '{fileName}'");
+                    return;
                 }
 
                 switch (check)
                 {
+                    case -2:
+                        QuickLogger.Error($"Unexpected error when attempting to parse file '{fileName}'");
+                        break;
                     case -1:
                         QuickLogger.Error($"Unable to parse file '{fileName}'");
                         break;
@@ -136,70 +107,6 @@
             {
                 QuickLogger.Warning($"Could not identify primary key in file '{fileName}'");
             }
-        }
-
-        private static int ParseEntries<T, T2>(string serializedData, ICollection<T> parsedItems)
-            where T : EmPropertyCollection, ICustomCraft
-            where T2 : EmPropertyCollectionList<T>, new()
-        {
-            var list = new T2();
-
-            bool successfullyParsed = list.Deserialize(serializedData);
-
-            if (!successfullyParsed)
-                return -1; // Error case
-
-            if (list.Count == 0)
-                return 0; // No entries
-
-            int count = 0;
-            foreach (T item in list)
-            {
-                parsedItems.Add(item);
-                count++;
-            }
-
-            return count; // Return the number of unique entries added in this list
-        }
-
-        private static void PrePassValidation<T>(ICollection<T> entries, IDictionary<string, T> uniqueEntries) where T : ICustomCraft
-        {
-            string typeName = typeof(T).Name;
-
-            //  Use the ToSet function as a copy constructor - this way we can iterate across the
-            //      temp structure, but change the permanent one in the case of duplicates
-            foreach (T item in entries)
-            {
-                if (!item.PassesPreValidation())
-                    continue;
-
-                if (uniqueEntries.ContainsKey(item.ID))
-                {
-                    QuickLogger.Warning($"Duplicate entry for {typeName} '{item.ID}' was already added by another working file. Kept first one. Discarded duplicate.");
-                    continue;
-                }
-
-                // All checks passed
-                uniqueEntries.Add(item.ID, item);
-            }
-
-            if (entries.Count > 0)
-            {
-                QuickLogger.Message($"{uniqueEntries.Count} of {entries.Count} {typeName} entries staged for patching");
-            }
-        }
-
-        private static void SendToSMLHelper<T>(ICollection<T> uniqueEntries) where T : ICustomCraft
-        {
-            int successCount = 0;
-            foreach (T item in uniqueEntries)
-            {
-                if (item.SendToSMLHelper())
-                    successCount++;
-            }
-
-            if (uniqueEntries.Count > 0)
-                QuickLogger.Message($"{successCount} of {uniqueEntries.Count} {typeof(T).Name} entries were patched");
         }
     }
 }
---
+++ CustomCraftSML/Serialization/IParsingPackage.cs
@@ -0,0 +1,10 @@
+﻿namespace CustomCraft2SML.Serialization
+{
+    internal interface IParsingPackage
+    {
+        int ParseEntries(string serializedData);
+        void PrePassValidation();
+        void SendToSMLHelper();
+        string ListKey { get; }
+    }
+}
