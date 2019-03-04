69a4cb5bff92730b69331a62f318602967ab8d69
doriannaujokat <35139623+doriannaujokat@users.noreply.github.com>
2019-03-04 02:07:00 +0100

    CC2_CustomFood - Rebuild of CustomFood from ground up

--- CustomCraftSML/Interfaces/ICustomFood.cs
+++ CustomCraftSML/Interfaces/ICustomFood.cs
@@ -1,9 +1,29 @@
-﻿using Common.EasyMarkup;
+﻿using System.Collections.Generic;
+using Common.EasyMarkup;
+using CustomCraft2SML.Serialization.Components;
 
 namespace CustomCraft2SML.Interfaces
 {
-    interface ICustomFood : IAliasRecipe
+    interface ICustomFood : ITechTyped
     {
+        short AmountCrafted { get; }
+        bool ForceUnlockAtStart { get; }
+
+        IList<EmIngredient> Ingredients { get; }
+        IList<string> LinkedItemIDs { get; }
+        IList<string> Unlocks { get; }
+        IList<string> UnlockedBy { get; }
+
+        string DisplayName { get; }
+        string Tooltip { get; }
+        TechType SpriteItemID { get; }
+
+        string Path { get; }
+        TechGroup PdaGroup { get; }
+        TechCategory PdaCategory { get; }
+
+        //FOOD SPECIFIC VALUES
+
         short FoodValue { get; }
         short WaterValue { get; }
         bool Decomposes{ get; }
--- CustomCraftSML/Serialization/Entries/CustomFood.cs
+++ CustomCraftSML/Serialization/Entries/CustomFood.cs
@@ -1,4 +1,9 @@
-﻿namespace CustomCraft2SML.Serialization.Entries
+﻿using CustomCraft2SML.Interfaces.InternalUse;
+using CustomCraft2SML.Serialization.Components;
+using SMLHelper.V2.Crafting;
+using SMLHelper.V2.Handlers;
+
+namespace CustomCraft2SML.Serialization.Entries
 {
     using Common;
     using Common.EasyMarkup;
@@ -9,131 +14,305 @@
     using System;
     using System.Collections.Generic;
 
-    internal class CustomFood : AliasRecipe, ICustomFood
+    internal class CustomFood : EmTechTyped, ICustomFood, ICustomCraft
     {
-        internal static new readonly string[] TutorialText = new[]
+
+        public OriginFile Origin { get; set; }
+
+        public short AmountCrafted
         {
-            $"{CustomFoodList.ListKey}: Create a custom Eatable.",
-            $"    FoodValue: Must be a value between {Min} and {Max}",
-            $"    WaterValue: Must be a value between {Min} and {Max}",
-            $"    Decomposes: Must be either YES or NO",
-            $"    DecayRate: Must be a value between X and X",
-        };
+            get => emAmount.Value;
+            set => emAmount.Value = value;
+        }
+        protected readonly EmProperty<short> emAmount;
+        protected const string AmountCraftedKey = "AmountCrafted";
 
-        public const short Max = 100;
-        public const short Min = 0;
+        public bool ForceUnlockAtStart
+        {
+            get => emForceAtStart.Value;
+            set => emForceAtStart.Value = value;
+        }
+        protected readonly EmYesNo emForceAtStart;
+        protected const string ForceUnlockKey = "ForceUnlockAtStart";
 
-        private const string FoodValueKey = "FoodValue";
-        private const string WaterValueKey = "WaterValue";
-        private const string DecomposesKey = "Decomposes";
-        private const string DecayRateKey = "DecayRate";
+        public IList<EmIngredient> Ingredients => emIngredient.Values;
+        protected readonly EmPropertyCollectionList<EmIngredient> emIngredient;
+        protected List<Ingredient> IngredientsItems { get; } = new List<Ingredient>();
+        protected const string IngredientsKey = "Ingredients";
 
-        protected readonly EmProperty<short> emFood;
-        protected readonly EmProperty<short> emWater;
-        protected readonly EmYesNo emDecomp;
-        protected readonly EmProperty<short> emDecayR;
+        public IList<string> LinkedItemIDs => emLinked.Values;
+        protected readonly EmPropertyList<string> emLinked;
+        protected List<TechType> LinkedItems { get; } = new List<TechType>();
+        protected const string LinkedKey = "LinkedItemIDs";
+
+        public IList<string> Unlocks => emUnlocks.Values;
+        protected readonly EmPropertyList<string> emUnlocks;
+        protected List<TechType> UnlocksItems { get; } = new List<TechType>();
+        protected const string UnlocksKey = "Unlocks";
+
+        public IList<string> UnlockedBy => emUnlockedby.Values;
+        protected readonly EmPropertyList<string> emUnlockedby;
+        protected List<TechType> UnlockedByItems { get; } = new List<TechType>();
+        protected const string UnlockedbyKey = "UnlockedBy";
+
+        public string DisplayName
+        {
+            get => emDisplayname.Value;
+            set => emDisplayname.Value = value;
+        }
+        protected readonly EmProperty<string> emDisplayname;
+        protected const string DisplayNameKey = "DisplayName";
+
+        public string Tooltip
+        {
+            get => emTooltip.Value;
+            set => emTooltip.Value = value;
+        }
+        protected readonly EmProperty<string> emTooltip;
+        protected const string TooltipKey = "Tooltip";
+
+        public TechType SpriteItemID
+        {
+            get => emSpriteID.Value;
+            set => emSpriteID.Value = value;
+        }
+        protected readonly EmProperty<TechType> emSpriteID;
+        protected const string SpriteKey = "SpriteItemID";
+
+        public string Path
+        {
+            get => emPath.Value;
+            set => emPath.Value = value;
+        }
+        protected readonly EmProperty<string> emPath;
+        protected const string PathKey = "Path";
+
+        public TechGroup PdaGroup
+        {
+            get => emPdagroup.Value;
+            set => emPdagroup.Value = value;
+        }
+        protected readonly EmProperty<TechGroup> emPdagroup;
+        protected const string PdagroupKey = "PdaGroup";
+
+        public TechCategory PdaCategory
+        {
+            get => emPdacategory.Value;
+            set => emPdacategory.Value = value;
+        }
+        protected readonly EmProperty<TechCategory> emPdacategory;
+        protected const string PdacategoryKey = "PdaCategory";
 
         public short FoodValue
         {
             get => emFood.Value;
-            set
-            {
-                if (value > Max || value < Min)
-                    value = emFood.DefaultValue;
-
-                emFood.Value = value;
-            }
+            set => emFood.Value = value;
         }
+        protected readonly EmProperty<short> emFood;
+        protected const string FoodKey = "FoodValue";
 
         public short WaterValue
         {
             get => emWater.Value;
-            set
-            {
-                if (value > Max || value < Min)
-                    value = emWater.DefaultValue;
-
-                emWater.Value = value;
-            }
+            set => emWater.Value = value;
         }
+        protected readonly EmProperty<short> emWater;
+        protected const string WaterKey = "WaterValue";
 
         public bool Decomposes
         {
-            get
-            {
-                if (emDecomp.HasValue)
-                    return emDecomp.Value;
-
-                return emDecomp.DefaultValue;
-            }
-            set => emDecomp.Value = value;
+            get => emDecomposes.Value;
+            set => emDecomposes.Value = value;
         }
+        protected readonly EmYesNo emDecomposes;
+        protected const string DecomposesKey = "Decomposes";
 
         public short DecayRate
         {
-            get
-            {
-                if (emDecayR.HasValue)
-                    return emDecayR.Value;
-
-                return emDecayR.DefaultValue;
-            }
-            set => emDecayR.Value = value;
+            get => emDecayrate.Value;
+            set => emDecayrate.Value = value;
         }
+        protected readonly EmProperty<short> emDecayrate;
+        protected const string DecayrateKey = "DecayRate";
 
-        protected static List<EmProperty> FoodProperties => new List<EmProperty>(AliasRecipeProperties)
+        public string ID => this.ItemID;
+        //protected readonly EmProperty<string> emId;
+        //protected const string IdKey = "ID";
+
+
+        public const string TypeName = "CustomFood";
+
+
+        protected static List<EmProperty> CustomFoodProperties => new List<EmProperty>(TechTypedProperties)
         {
-            new EmProperty<short>(FoodValueKey, 0),
-            new EmProperty<short>(WaterValueKey, 0),
-            new EmYesNo(DecomposesKey, false) { Optional = true },
-            new EmProperty<short>(DecayRateKey, 1) { Optional = true } // TODO find out what this actually is
+            new EmProperty<short>(AmountCraftedKey, 1) { Optional = true },
+            new EmYesNo(ForceUnlockKey, true) { Optional = true },
+            new EmPropertyCollectionList<EmIngredient>(IngredientsKey) { Optional = true },
+            new EmPropertyList<string>(LinkedKey) { Optional = true },
+            new EmPropertyList<string>(UnlocksKey) { Optional = true },
+            new EmPropertyList<string>(UnlockedbyKey) { Optional = true },
+            new EmProperty<string>(DisplayNameKey) { Optional = true },
+            new EmProperty<string>(TooltipKey) { Optional = true },
+            new EmProperty<TechType>(SpriteKey) { Optional = true },
+            new EmProperty<string>(PathKey) { Optional = true },
+            new EmProperty<TechGroup>(PdagroupKey) { Optional = true },
+            new EmProperty<TechCategory>(PdacategoryKey) { Optional = true },
+            new EmProperty<short>(FoodKey) { Optional = false },
+            new EmProperty<short>(WaterKey) { Optional = false },
+            new EmYesNo(DecomposesKey) { Optional = true },
+            new EmProperty<short>(DecayrateKey) { Optional = true },
         };
 
-        public CustomFood() : this("CustomFood", FoodProperties)
+
+        internal CustomFood(TechType origTechType) : this()
+        {
+            ITechData origRecipe = CraftData.Get(origTechType);
+            this.ItemID = origTechType.ToString();
+            this.AmountCrafted = (short)origRecipe.craftAmount;
+
+            for (int i = 0; i < origRecipe.ingredientCount; i++)
+            {
+                IIngredient origIngredient = origRecipe.GetIngredient(i);
+                this.Ingredients.Add(new EmIngredient(origIngredient.techType, (short)origIngredient.amount));
+            }
+
+            for (int i = 0; i < origRecipe.linkedItemCount; i++)
+                emLinked.Add(origRecipe.GetLinkedItem(i).ToString());
+        }
+
+        public CustomFood() : this(TypeName, CustomFoodProperties)
+        {
+        }
+
+        protected CustomFood(string key) : this(key, CustomFoodProperties)
         {
         }
 
         protected CustomFood(string key, ICollection<EmProperty> definitions) : base(key, definitions)
         {
-            emFood = (EmProperty<short>)Properties[FoodValueKey];
-            emWater = (EmProperty<short>)Properties[WaterValueKey];
-            emDecomp = (EmYesNo)Properties[DecomposesKey];
-            emDecayR = (EmProperty<short>)Properties[DecayRateKey];
+            emAmount = (EmProperty<short>) Properties[AmountCraftedKey];
+            emForceAtStart = (EmYesNo) Properties[ForceUnlockKey];
+            emIngredient = (EmPropertyCollectionList<EmIngredient>) Properties[IngredientsKey];
+            emLinked = (EmPropertyList<string>) Properties[LinkedKey];
+            emUnlocks = (EmPropertyList<string>) Properties[UnlocksKey];
+            emUnlockedby = (EmPropertyList<string>) Properties[UnlockedbyKey];
+            emDisplayname = (EmProperty<string>) Properties[DisplayNameKey];
+            emTooltip = (EmProperty<string>) Properties[TooltipKey];
+            emSpriteID = (EmProperty<TechType>) Properties[SpriteKey];
+            emPath = (EmProperty<string>) Properties[PathKey];
+            emPdagroup = (EmProperty<TechGroup>) Properties[PdagroupKey];
+            emPdacategory = (EmProperty<TechCategory>) Properties[PdacategoryKey];
+            emFood = (EmProperty<short>) Properties[FoodKey];
+            emWater = (EmProperty<short>) Properties[WaterKey];
+            emDecomposes = (EmYesNo) Properties[DecomposesKey];
+            emDecayrate = (EmProperty<short>) Properties[DecayrateKey];
+
+            OnValueExtractedEvent += ValueExtracted;
+        }
+
+        private void ValueExtracted()
+        {
+            foreach (EmIngredient ingredient in emIngredient)
+            {
+                string itemID = (ingredient["ItemID"] as EmProperty<string>).Value;
+                short required = (ingredient["Required"] as EmProperty<short>).Value;
+            }
         }
 
         internal override EmProperty Copy() => new CustomFood(this.Key, this.CopyDefinitions);
 
-        public override bool PassesPreValidation() => InnerItemsAreValid() && FunctionalItemIsValid() & ValidateFoods();
 
-        private bool ValidateFoods()
-        {
-            if (this.FoodValue < Min || this.WaterValue < Min || this.FoodValue > Max || this.WaterValue > Max)
-            {
-                QuickLogger.Error(
-                    $"Error in {this.Key} for '{this.ItemID}' from {this.Origin}. Eatable values must be between between {Min} and {Max}.");
-                return false;
-            }
 
-            return true;
-        }
-
-        public override bool SendToSMLHelper()
+        public bool SendToSMLHelper()
         {
             try
             {
-                TechType baseType = this.Decomposes ? TechType.CookedPeeper : TechType.CuredPeeper;
-                var craftPath = new CraftingPath(this.Path, this.ItemID);
-
-                var food = new CustomFoodCraftable(this, craftPath, baseType);
-                food.Patch();
-
-                return true;
+                
             }
             catch (Exception ex)
             {
-                QuickLogger.Error($"Exception thrown while handling {this.Key} '{this.ItemID}' from {this.Origin}", ex);
+                QuickLogger.Error($"Exception thrown while handling {this.Key} entry '{this.ItemID}' from {this.Origin}", ex);
                 return false;
             }
+        }
+
+
+        private bool ValidateUnlockedBy()
+        {
+            bool unlockedByValid = true;
+
+            foreach (string unlockedBy in this.UnlockedBy)
+            {
+                TechType unlockByItemID = GetTechType(unlockedBy);
+
+                if (unlockByItemID == TechType.None)
+                {
+                    QuickLogger.Warning($"{this.Key} entry with ID of '{this.ItemID}' contained an unknown {UnlockedBy} '{unlockedBy}'. Entry will be discarded.");
+                    unlockedByValid = false;
+                    continue;
+                }
+
+                this.UnlockedByItems.Add(unlockByItemID);
+            }
+
+            return unlockedByValid;
+        }
+
+        private bool ValidateUnlocks()
+        {
+            bool unlocksValid = true;
+
+            foreach (string unlockingItem in this.Unlocks)
+            {
+                TechType unlockingItemID = GetTechType(unlockingItem);
+
+                if (unlockingItemID == TechType.None)
+                {
+                    QuickLogger.Warning($"{this.Key} entry with ID of '{this.ItemID}' contained an unknown {UnlocksKey} '{unlockingItem}'. Entry will be discarded.");
+                    unlocksValid = false;
+                    continue;
+                }
+
+                this.UnlocksItems.Add(unlockingItemID);
+            }
+
+            return unlocksValid;
+        }
+
+        private bool ValidateLinkedItems()
+        {
+            bool linkedItemsValid = true;
+
+            foreach (string linkedItem in this.LinkedItemIDs)
+            {
+                TechType linkedItemID = GetTechType(linkedItem);
+
+                if (linkedItemID == TechType.None)
+                {
+                    QuickLogger.Warning($"{this.Key} entry '{this.ItemID}' from {this.Origin} contained an unknown {LinkedItemsIdsKey} '{linkedItem}'. Entry will be discarded.");
+                    linkedItemsValid = false;
+                    continue;
+                }
+
+                this.LinkedItems.Add(linkedItemID);
+            }
+
+            return linkedItemsValid;
+        }
+
+        private bool ValidateIngredients()
+        {
+            bool ingredientsValid = true;
+
+            foreach (EmIngredient ingredient in this.Ingredients)
+            {
+                if (ingredient.PassesPreValidation())
+                    this.IngredientsItems.Add(ingredient.ToSMLHelperIngredient());
+                else
+                    ingredientsValid = false;
+            }
+
+            return ingredientsValid;
         }
 
     }
