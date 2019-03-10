520e2c431c17ea53f7bfd2ae2552464fa2706fcd
PrimeSonic <prime.sonic@gmail.com>
2019-03-10 11:44:09 -0400

    MCU_PowerInfo - Correction to console HUD refresh

--- MoreCyclopsUpgrades/Patchers/CyclopsUpgradeConsoleHUDManager_RefreshScreen_Patcher.cs
+++ MoreCyclopsUpgrades/Patchers/CyclopsUpgradeConsoleHUDManager_RefreshScreen_Patcher.cs
@@ -7,19 +7,17 @@
     [HarmonyPatch("RefreshScreen")]
     internal class CyclopsUpgradeConsoleHUDManager_RefreshScreen_Patcher
     {
-        [HarmonyPrefix]
-        public static bool Prefix(ref CyclopsUpgradeConsoleHUDManager __instance)
+        [HarmonyPostfix]
+        public static void Postfix(ref CyclopsUpgradeConsoleHUDManager __instance)
         {
             CyclopsHUDManager hudMgr = CyclopsManager.GeHUDManager(__instance.subRoot);
 
             if (hudMgr == null)
             {
-                return true;
+                return;
             }
 
             hudMgr.UpdateConsoleHUD(__instance);
-
-            return false;
         }
     }
 }
