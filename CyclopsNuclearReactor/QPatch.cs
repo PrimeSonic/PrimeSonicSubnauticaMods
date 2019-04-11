9fefdcbca333c7e496ac0f3ecb2daa3eeccf4afe
PrimeSonic <prime.sonic@gmail.com>
2019-04-10 22:20:02 -0400

    CyclopsNuclearReactor - Initial project files

---
+++ CyclopsNuclearReactor/QPatch.cs
@@ -0,0 +1,31 @@
+﻿namespace CyclopsNuclearReactor
+{
+    using System;
+    using System.Reflection;
+    using Common;
+    using Harmony;
+
+    public static class QPatch
+    {
+        public static void Patch()
+        {
+            QuickLogger.Info("Started patching. Version: " + QuickLogger.GetAssemblyVersion());
+
+#if DEBUG
+            QuickLogger.DebugLogsEnabled = true;
+            QuickLogger.Debug("Debug logs enabled");
+#endif
+
+            try
+            {
+                var harmony = HarmonyInstance.Create("com.cyclopsnuclearreactor.psmod");
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+                QuickLogger.Info("Finished patching");
+            }
+            catch (Exception ex)
+            {
+                QuickLogger.Error(ex);
+            }
+        }
+    }
+}
