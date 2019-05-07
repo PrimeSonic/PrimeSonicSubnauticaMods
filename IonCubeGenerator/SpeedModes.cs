e2d991995b1cd7987742fda4243f78d7ba6e2ee5
PrimeSonic <39146191+PrimeSonic@users.noreply.github.com>
2019-05-07 06:52:49 -0400

    Update SpeedModes.cs
    Corrected the comments indicating time calculations.

--- IonCubeGenerator/SpeedModes.cs
+++ IonCubeGenerator/SpeedModes.cs
@@ -3,9 +3,9 @@
     internal enum SpeedModes : int
     {
         Off = 0,  // Not generating
-        Max = 25,  // 2.5 minutes
-        High = 50, // 5 minutes
-        Low = 75,  // 7.5 minutes
-        Min = 100, // 10 minutes
+        Max = 25,  // 250 seconds ~ 4.1 minutes
+        High = 50, // 500 seconds ~ 8.3 minutes
+        Low = 75,  // 750 seconds ~ 12.5 minutes
+        Min = 100, // 1000 seconds ~ 16.6 minutes
     }
 }
