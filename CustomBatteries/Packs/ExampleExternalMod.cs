911e2a81220419381d351bde99ff8678181a58a4
PrimeSonic <39146191+PrimeSonic@users.noreply.github.com>
2019-12-03 15:11:22 -0500

    CustomBatteries
    Updated `ExampleExternalMod` with better comments and spelling fixes

--- CustomBatteries/Packs/ExampleExternalMod.cs
+++ CustomBatteries/Packs/ExampleExternalMod.cs
@@ -1,13 +1,17 @@
 ﻿#if DEBUG
 namespace CustomBatteries.Packs
 {
+    // EXAMPLE MOD - Using CustomBatteries from an external mod
     using System.Collections.Generic;
     using CustomBatteries.API;
 
+    // Step 0 - You will need to make your own class that implements the IModPluginPack interface.
+    // It can be as simple as this example right here.
     internal class MyPack : IModPluginPack
     {
+        // ProTip: You can load your own sprites duringly from PNG files using SMLHelper's ImageUtils class.
         public Atlas.Sprite BatteryIcon { get; } = SpriteManager.Get(TechType.Battery);
-        public Atlas.Sprite PowerCellIcon { get; } = SpriteManager.Get(TechType.PowerCell);
+        public Atlas.Sprite PowerCellIcon { get; } = SpriteManager.Get(TechType.PowerCell);        
         public string PluginPackName { get; } = "Example Plugin";
         public int BatteryCapacity { get; } = 120;
         public TechType UnlocksWith { get; } = TechType.Shocker;
@@ -25,16 +29,16 @@ namespace CustomBatteries.Packs
     {
         public static void Patch()
         {
-            // Sending your mod pack to CustomBatteries
-
             // Step 1 - Instantiate the service class
             var service = new CustomBatteriesService();
 
-            // Step 2 - Get your pluging pack
+            // Step 2 - Get your plugin pack intance
             var myPack = new MyPack();
 
-            // Step 3 - Call the AddPluginPackFromMod method with your pluging pack
+            // Step 3 - Call the AddPluginPackFromMod method with your plugin pack            
             service.AddPluginPackFromMod(myPack);
+                        
+            // Do whatever other patching your mod requires
         }
     }
 }
