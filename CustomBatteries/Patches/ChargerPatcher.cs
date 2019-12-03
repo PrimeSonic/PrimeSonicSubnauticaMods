04a7ec063cb0678f62a68db0943f0edc99df7bc8
PrimeSonic <prime.sonic@gmail.com>
2019-12-03 14:52:28 -0500

    CustomBatteries - Correcting major bug in harmony patches of ChargerPatcher

--- CustomBatteries/Patches/ChargerPatcher.cs
+++ CustomBatteries/Patches/ChargerPatcher.cs
@@ -3,6 +3,7 @@
     using System.Collections.Generic;
     using System.Reflection;
     using Common;
+    using CustomBatteries.Items;
     using Harmony;
 
     public static class ChargerPatcher
@@ -14,10 +15,21 @@
             MethodInfo batteryChargerInitializeMethod = AccessTools.Method(typeof(BatteryCharger), nameof(BatteryCharger.Initialize));
             MethodInfo powerCellChargerInitializeMethod = AccessTools.Method(typeof(PowerCellCharger), nameof(PowerCellCharger.Initialize));
 
-            MethodInfo initializePreFixMethod = AccessTools.Method(typeof(ChargerPatcher), nameof(ChargerPatcher.InitializePrefix));
+            MethodInfo initializeBatteryChargerPreFix = AccessTools.Method(typeof(ChargerPatcher), nameof(ChargerPatcher.PrefixBatteryCharger));
+            MethodInfo initializePowerCellChargerPrefix = AccessTools.Method(typeof(ChargerPatcher), nameof(ChargerPatcher.PrefixPowerCellCharger));
 
-            harmony.Patch(batteryChargerInitializeMethod, prefix: new HarmonyMethod(initializePreFixMethod)); // Patches the BatteryCharger Initialize method
-            harmony.Patch(powerCellChargerInitializeMethod, prefix: new HarmonyMethod(initializePreFixMethod)); // Patches the PowerCellCharger Initialize method
+            harmony.Patch(batteryChargerInitializeMethod, prefix: new HarmonyMethod(initializeBatteryChargerPreFix)); // Patches the BatteryCharger Initialize method
+            harmony.Patch(powerCellChargerInitializeMethod, prefix: new HarmonyMethod(initializePowerCellChargerPrefix)); // Patches the PowerCellCharger Initialize method
+        }
+
+        public static void PrefixBatteryCharger()
+        {
+            InitializePrefix(BatteryCharger.compatibleTech, CbCore.BatteryTechTypes);
+        }
+
+        public static void PrefixPowerCellCharger()
+        {
+            InitializePrefix(PowerCellCharger.compatibleTech, CbCore.PowerCellTechTypes);
         }
 
         public static void InitializePrefix(HashSet<TechType> compatibleTech, List<TechType> toBeAdded)
@@ -28,6 +40,7 @@
             // Make sure all custom batteries are allowed in the battery charger
             if (!compatibleTech.Contains(toBeAdded[toBeAdded.Count - 1]))
             {
+                // Checks in reverse order to account for the (unlikely) event that an external mod patches later than expected
                 for (int i = toBeAdded.Count - 1; i >= 0; i--)
                 {
                     TechType entry = toBeAdded[i];
