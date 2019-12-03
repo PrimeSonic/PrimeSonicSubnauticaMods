41bba24b4661ae4a86d3897f3c1276911aa3f34d
PrimeSonic <prime.sonic@gmail.com>
2019-12-03 11:44:59 -0500

    CustomBatteries - Added missing XML documentation

--- CustomBatteries/API/IModPluginPack.cs
+++ CustomBatteries/API/IModPluginPack.cs
@@ -6,8 +6,20 @@
     /// <seealso cref="IPluginPack" />
     public interface IModPluginPack : IPluginPack
     {
+        /// <summary>
+        /// Gets the battery icon sprite.
+        /// </summary>
+        /// <value>
+        /// The battery icon.
+        /// </value>
         Atlas.Sprite BatteryIcon { get; }
 
+        /// <summary>
+        /// Gets the power cell icon sprite.
+        /// </summary>
+        /// <value>
+        /// The power cell icon.
+        /// </value>
         Atlas.Sprite PowerCellIcon { get; }
     }
 }
