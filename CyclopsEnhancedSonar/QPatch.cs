2ea8740c333e894337e484a6a966aeda12ecab49
PrimeSonic <prime.sonic@gmail.com>
2019-12-14 01:13:44 -0500

    CyclopsEnhancedSonar - adapted from frigidpenguin's original CyclopsNearFieldSonar - Adds a local map display to the helm similar to the seaglide - Adding a second sonar upgrade will speed up the pings

---
+++ CyclopsEnhancedSonar/QPatch.cs
@@ -0,0 +1,39 @@
+﻿namespace CyclopsEnhancedSonar
+{
+    using System;
+    using System.Reflection;
+    using Common;
+    using Harmony;
+    using MoreCyclopsUpgrades.API;
+
+    public static class QPatch
+    {
+        public static void Patch()
+        {
+            QuickLogger.Info($"Started patching. Version {QuickLogger.GetAssemblyVersion()}");
+
+            try
+            {                
+                MethodInfo subControlStartMethod = AccessTools.Method(typeof(SubControl), nameof(SubControl.Start));
+
+                var harmony = HarmonyInstance.Create("com.CyclopsEnhancedSonar.psmod");
+                harmony.Patch(subControlStartMethod, postfix: new HarmonyMethod(typeof(QPatch), nameof(QPatch.SubControlStartPostfix)));
+                
+                MCUServices.Register.CyclopsUpgradeHandler((SubRoot cyclops) => new SonarUpgradeHandler(cyclops));
+                MCUServices.Register.PdaIconOverlay(TechType.CyclopsSonarModule, (uGUI_ItemIcon icon, InventoryItem upgradeModule) => new SonarPdaDisplay(icon, upgradeModule));
+
+                QuickLogger.Info($"Finished patching.");
+            }
+            catch (Exception ex)
+            {
+                QuickLogger.Error(ex);
+            }
+        }
+
+        public static void SubControlStartPostfix(SubControl __instance)
+        {
+            if (__instance.gameObject.name.StartsWith("Cyclops-MainPrefab"))
+                __instance.gameObject.AddComponent<CySonarComponent>();
+        }
+    }
+}
---
+++ CyclopsEnhancedSonar/SonarPdaDisplay.cs
@@ -0,0 +1,21 @@
+﻿namespace CyclopsEnhancedSonar
+{
+    using MoreCyclopsUpgrades.API;
+    using MoreCyclopsUpgrades.API.PDA;
+
+    internal class SonarPdaDisplay : IconOverlay
+    {
+        public SonarPdaDisplay(uGUI_ItemIcon icon, InventoryItem upgradeModule)
+            : base(icon, upgradeModule)
+        {
+        }
+
+        public override void UpdateText()
+        {
+            int upgradeCount = MCUServices.CrossMod.GetUpgradeCount(base.Cyclops, TechType.CyclopsSonarModule);
+
+            base.LowerText.FontSize = 14 + (3 * upgradeCount);
+            base.LowerText.TextString = $"{upgradeCount}/{SonarUpgradeHandler.MaxUpgrades}";
+        }
+    }
+}
