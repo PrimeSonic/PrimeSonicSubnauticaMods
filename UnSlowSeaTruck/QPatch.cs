52db24db0a904863107fe4e73c198293b9516367
PrimeSonic <prime.sonic@gmail.com>
2020-05-20 08:11:26 -0400

    UnSlowSeaTruck - Minor updates and QMM 3 loading - Making ConfigMgr more reusable for later

--- UnSlowSeaTruck/QPatch.cs
+++ UnSlowSeaTruck/QPatch.cs
@@ -3,45 +3,56 @@
     using System;
     using System.Reflection;
     using Harmony;
+    using QModManager.API.ModLoading;
 
-    public class QPatch
+    [QModCore]
+    public static class QPatch
     {
+        internal static SeaTruckConfig Config;
+
+        [QModPatch]
         public static void Patch()
         {
-            Console.WriteLine("[UnSlowSeaTruck] Patching v" + Assembly.GetExecutingAssembly().GetName().Version.ToString(3));
+            var assembly = Assembly.GetExecutingAssembly();
 
-            var cfgMgr = new ConfigMgr();
-            cfgMgr.LoadConfig();
+            Console.WriteLine("[UnSlowSeaTruck] Patching v" + assembly.GetName().Version.ToString(3));
 
-            var harmony = HarmonyInstance.Create("com.unslowseatruck.psmod");
-            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            Config = ConfigMgr.LoadConfig<SeaTruckConfig>();
+            Console.WriteLine($"[UnSlowSeaTruck] Configured WeightOverride: {Config.WeightOverride}");
+
+            var harmony = HarmonyInstance.Create($"com.{assembly.GetName().Name}.psmod");
+            harmony.PatchAll(assembly);
         }
     }
 
-    [HarmonyPatch(typeof(SeaTruckSegment))]
-    [HarmonyPatch(nameof(SeaTruckSegment.GetAttachedWeight))]
+    [HarmonyPatch(typeof(SeaTruckSegment), nameof(SeaTruckSegment.GetAttachedWeight))]
     internal static class SeaTruckSegment_GetAttachedWeight_Override
     {
         [HarmonyPrefix]
         internal static bool Prefix(ref float __result)
         {
-            __result = ConfigMgr.Config.WeightOverride; // Override attached module weight
+            __result = QPatch.Config.WeightOverride; // Override attached module weight
 
             return false;
         }
     }
 
-    [HarmonyPatch(typeof(SeaTruckMotor))]
-    [HarmonyPatch(nameof(SeaTruckMotor.Start))]
+    [HarmonyPatch(typeof(SeaTruckMotor), nameof(SeaTruckMotor.Start))]
     internal static class SeatruckMotor_Start_Change
     {
         [HarmonyPostfix]
         internal static void Postfix(ref SeaTruckMotor __instance)
         {
-            SeaTruckConfig config = ConfigMgr.Config;
+            SeaTruckConfig config = QPatch.Config;
+
+            Console.WriteLine($"[UnSlowSeaTruck] Original steeringMultiplier: {__instance.steeringMultiplier}");
+            Console.WriteLine($"[UnSlowSeaTruck] Original acceleration: {__instance.acceleration}");
 
             __instance.steeringMultiplier *= config.SteeringMultiplier; // better steering
             __instance.acceleration *= config.AccelerationMultiplier; // better acceleration
+
+            Console.WriteLine($"[UnSlowSeaTruck] Modified steeringMultiplier: {__instance.steeringMultiplier} (x{config.SteeringMultiplier})");
+            Console.WriteLine($"[UnSlowSeaTruck] Modified acceleration: {__instance.acceleration} (x{config.AccelerationMultiplier})");
         }
     }
 }
--- UnSlowSeaTruck/SeaTruckConfig.cs
+++ UnSlowSeaTruck/SeaTruckConfig.cs
@@ -5,13 +5,10 @@
     [JsonObject]
     public class SeaTruckConfig
     {
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
         public float SteeringMultiplier { get; set; } = 1.17f;
 
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
         public float AccelerationMultiplier { get; set; } = 1.28f;
 
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
         public float WeightOverride { get; set; } = 0.0001f;
     }
 }
