33dd60f83181cef15c63852d5a001e8bc8158a80
PrimeSonic <prime.sonic@gmail.com>
2020-05-21 20:55:39 -0400

    UnSlowSeaTruck v1.2 - Added HorsePowerModifier to config

--- UnSlowSeaTruck/ConfigMgr.cs
+++ UnSlowSeaTruck/ConfigMgr.cs
@@ -7,12 +7,12 @@
 
     internal class ConfigMgr
     {
-        internal static TConfig LoadConfig<TConfig>(string fileName = null) where TConfig : class, new()
-        {
-            var assembly = Assembly.GetCallingAssembly();
+        private static string ModName = Assembly.GetExecutingAssembly().GetName().Name;
+        private static readonly string ModDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
-            string modDirectory = Path.GetDirectoryName(assembly.Location);
-            string configFilePath = Path.Combine(modDirectory, fileName ?? $"{typeof(TConfig).Name}.json");
+        internal static TConfig LoadConfig<TConfig>() where TConfig : class, new()
+        {
+            string configFilePath = Path.Combine(ModDirectory, $"{typeof(TConfig).Name}.json");
 
             if (File.Exists(configFilePath))
             {
@@ -22,32 +22,38 @@
 
                     if (string.IsNullOrEmpty(serialilzedConfig))
                     {
-                        Console.WriteLine($"[{assembly.GetName().Name}] Config file was empty. Overwriting with default values.");
+                        Console.WriteLine($"[{ModName}] Config file was empty. Overwriting with default values.");
                         return WriteDefaultConfig<TConfig>(configFilePath);
                     }
 
                     TConfig loadedConfig = JsonConvert.DeserializeObject<TConfig>(serialilzedConfig);
-                    Console.WriteLine($"[{assembly.GetName().Name}] Existing config file loaded.");
+                    Console.WriteLine($"[{ModName}] Existing config file loaded.");
                     return loadedConfig;
                 }
                 catch
                 {
-                    Console.WriteLine($"[{assembly.GetName().Name}] Failed to read config file. Overwriting with default values.");
+                    Console.WriteLine($"[{ModName}] Failed to read config file. Overwriting with default values.");
                     return WriteDefaultConfig<TConfig>(configFilePath);
                 }
             }
             else // Write default file
             {
-                Console.WriteLine($"[{assembly.GetName().Name}] No config file found. Writing default config file.");
+                Console.WriteLine($"[{ModName}] No config file found. Writing default config file.");
                 return WriteDefaultConfig<TConfig>(configFilePath);
             }
+        }
+
+        internal static void SaveConfig<TConfig>(TConfig config) where TConfig : class
+        {
+            string configFilePath = Path.Combine(ModDirectory, $"{typeof(TConfig).Name}.json");
+            string serialilzedConfig = JsonConvert.SerializeObject(config, Formatting.Indented);
+            File.WriteAllText(configFilePath, serialilzedConfig);
         }
 
         private static TConfig WriteDefaultConfig<TConfig>(string configFilePath) where TConfig : class, new()
         {
             var defaultConfig = new TConfig();
-            string serialilzedConfig = JsonConvert.SerializeObject(defaultConfig, Formatting.Indented);
-            File.WriteAllText(configFilePath, serialilzedConfig);
+            SaveConfig(defaultConfig);
             return defaultConfig;
         }
     }
