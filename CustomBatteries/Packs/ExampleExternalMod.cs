348d52d1642285f743500f25b0b45facab689483
PrimeSonic <prime.sonic@gmail.com>
2020-05-27 08:22:05 -0400

    CustomBatteries - clarifying ExampleExternalMod

--- CustomBatteries/Packs/ExampleExternalMod.cs
+++ CustomBatteries/Packs/ExampleExternalMod.cs
@@ -7,22 +7,22 @@ namespace CustomBatteries.Packs
 
     // Step 0 - You will need to make your own class that implements the IModPluginPack interface.
     // It can be as simple as this example right here.
-    internal class MyPack : IModPluginPack
+    internal class MyPlugin : IModPluginPack
     {
         // ProTip: You can load your own sprites duringly from PNG files using SMLHelper's ImageUtils class.
-        public Atlas.Sprite BatteryIcon { get; } = SpriteManager.Get(TechType.Battery);
-        public Atlas.Sprite PowerCellIcon { get; } = SpriteManager.Get(TechType.PowerCell);        
-        public string PluginPackName { get; } = "Example Plugin";
-        public int BatteryCapacity { get; } = 120;
-        public TechType UnlocksWith { get; } = TechType.Shocker;
-        public string BatteryID { get; } = "AmpBattery";
-        public string BatteryName { get; } = "Ampeel Battery";
-        public string BatteryFlavorText { get; } = "Recycle these annoying creatures into something more useful.";
-        public IList<TechType> BatteryParts { get; } = new List<TechType> { TechType.Shocker, TechType.Titanium, TechType.Copper };
-        public string PowerCellID { get; } = "AmpPowerCell";
-        public string PowerCellName { get; } = "Ampeel Power Cell";
-        public string PowerCellFlavorText { get; } = "Twice the power at twice the size!";
-        public IList<TechType> PowerCellAdditionalParts { get; } = new List<TechType>();
+        public Atlas.Sprite BatteryIcon => SpriteManager.Get(TechType.Battery);
+        public Atlas.Sprite PowerCellIcon => SpriteManager.Get(TechType.PowerCell);        
+        public string PluginPackName => "Example Plugin";
+        public int BatteryCapacity => 120;
+        public TechType UnlocksWith => TechType.Shocker;
+        public string BatteryID => "AmpBattery";
+        public string BatteryName => "Ampeel Battery";
+        public string BatteryFlavorText => "Recycle these annoying creatures into something more useful.";
+        public IList<TechType> BatteryParts => new List<TechType> { TechType.Shocker, TechType.Titanium, TechType.Copper };
+        public string PowerCellID => "AmpPowerCell";
+        public string PowerCellName => "Ampeel Power Cell";
+        public string PowerCellFlavorText => "Twice the power at twice the size!";
+        public IList<TechType> PowerCellAdditionalParts => new List<TechType> { TechType.Lithium };
     }
 
     internal static class MyQPatch
@@ -30,14 +30,19 @@ namespace CustomBatteries.Packs
         public static void Patch()
         {
             // Step 1 - Instantiate the service class
-            var service = new CustomBatteriesService();
+            var cbService = new CustomBatteriesService();
 
-            // Step 2 - Get your plugin pack intance
-            var myPack = new MyPack();
+            // Step 2 - Get your plugin pack instance
+            var myPlugin = new MyPlugin();
 
             // Step 3 - Call the AddPluginPackFromMod method with your plugin pack            
-            service.AddPluginPackFromMod(myPack);
-                        
+            CustomPack myPack = cbService.AddPluginPackFromMod(myPlugin);
+
+            // It will return a CustomPack that has everything you might need to continue patching.
+            // The ModPrefabs CustomBattery and CustomPowerCell are already patched for you.
+            TechType myBattery = myPack.CustomBattery.TechType;
+            TechType myPowercell = myPack.CustomPowerCell.TechType;
+
             // Do whatever other patching your mod requires
         }
     }
