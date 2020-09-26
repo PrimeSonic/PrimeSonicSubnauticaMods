289b4d29d71b3260641d87f0f2c088ecbc978c77
PrimeSonic <39146191+PrimeSonic@users.noreply.github.com>
2020-09-26 18:13:52 -0400

    CustomCraftSML hotfix v1.9.4 - Correction to FolderRoot issue

--- CustomCraftSML/FileLocations.cs
+++ CustomCraftSML/FileLocations.cs
@@ -8,13 +8,13 @@ namespace CustomCraft2SML
     {
         internal const string RootModName = "CustomCraft2SML";
         internal const string ModFriendlyName = "Custom Craft 2";
-        internal static string FolderRoot = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Assets");
-        internal static string SamplesFolder = FolderRoot + "SampleFiles/";
-        internal static string OriginalsFolder = FolderRoot + "OriginalRecipes/";
-        internal static string HowToFile = FolderRoot + "README_HowToUseThisMod.txt";
-        internal static string WorkingFolder = FolderRoot + "WorkingFiles/";
-        internal static string AssetsFolder = FolderRoot + "Assets/";
-        internal static string ModJson = FolderRoot + "mod.json";
-        internal static string ConfigFile = FolderRoot + CustomCraft2Config.FileName;
+        internal static string FolderRoot = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        internal static string SamplesFolder = Path.Combine(FolderRoot, "SampleFiles");
+        internal static string OriginalsFolder = Path.Combine(FolderRoot, "OriginalRecipes");
+        internal static string HowToFile = Path.Combine(FolderRoot, "README_HowToUseThisMod.txt");
+        internal static string WorkingFolder = Path.Combine(FolderRoot, "WorkingFiles");
+        internal static string AssetsFolder = Path.Combine(FolderRoot, "Assets");
+        internal static string ModJson = Path.Combine(FolderRoot, "mod.json");
+        internal static string ConfigFile = Path.Combine(FolderRoot, CustomCraft2Config.FileName);
     }
 }
